582de6d4fc963d8d4899fb0d2d06dfb991db5061
Ishita <ishiestar@gmail.com>
2016-02-16 11:40:21 +0530

    fixed recycling of listAdapter, transcriptMode not working

--- app/src/main/java/com/example/ishita/assigntasks/CommentsCursorAdapter.java
+++ app/src/main/java/com/example/ishita/assigntasks/CommentsCursorAdapter.java
@@ -9,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -22,28 +23,40 @@ public class CommentsCursorAdapter extends CursorAdapter {
         super(context, c, flags);
     }
 
+    public static class ViewHolder {
+        public final TextView message;
+        public final TextView timeStamp;
+        public final LinearLayout box;
+
+        public ViewHolder(View view) {
+            box = (LinearLayout) view.findViewById(R.id.box);
+            message = (TextView) view.findViewById(R.id.text1);
+            timeStamp = (TextView) view.findViewById(R.id.text2);
+        }
+    }
     /*@Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView =
+        }
         return super.getView(position, convertView, parent);
     }*/
 
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
         View listItem = LayoutInflater.from(context).inflate(R.layout.comments_list_item, parent, false);
-        TextView message = (TextView) listItem.findViewById(R.id.text1);
-        TextView timeStamp = (TextView) listItem.findViewById(R.id.text2);
-        message.setText(cursor.getString(cursor.getColumnIndex(TasksContract.MessageEntry.COL_MSG)));
-        timeStamp.setText(cursor.getString(cursor.getColumnIndex(TasksContract.MessageEntry.COL_AT)));
+        ViewHolder viewHolder = new ViewHolder(listItem);
+        listItem.setTag(viewHolder);
         return listItem;
     }
 
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
+        ViewHolder viewHolder = (ViewHolder) view.getTag();
         LinearLayout root = (LinearLayout) view;
         //TODO replace NULL in this check by the sender ID once login activity is done.
         if (cursor.getString(cursor.getColumnIndex(TasksContract.MessageEntry.COL_FROM)) == null) {
-            LinearLayout box = (LinearLayout) root.findViewById(R.id.box);
-            GradientDrawable sd = (GradientDrawable) box.getBackground().mutate();
+            GradientDrawable sd = (GradientDrawable) viewHolder.box.getBackground().mutate();
             sd.setColor(Color.CYAN);
             sd.invalidateSelf();
             root.setGravity(Gravity.END);
@@ -52,5 +65,7 @@ public class CommentsCursorAdapter extends CursorAdapter {
             root.setGravity(Gravity.LEFT);
             root.setPadding(10, 10, 50, 10);
         }
+        viewHolder.message.setText(cursor.getString(cursor.getColumnIndex(TasksContract.MessageEntry.COL_MSG)));
+        viewHolder.timeStamp.setText(cursor.getString(cursor.getColumnIndex(TasksContract.MessageEntry.COL_AT)));
     }
 }
