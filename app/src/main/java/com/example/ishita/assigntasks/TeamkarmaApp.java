067df416f44b5b02fdd7bd3f8414a430872762da
Ishita <ishiestar@gmail.com>
2016-03-24 08:55:08 +0530

    added activity lifecycle callbacks to check if CommentsActivity is in the foreground

--- app/src/main/java/com/example/ishita/assigntasks/TeamkarmaApp.java
+++ app/src/main/java/com/example/ishita/assigntasks/TeamkarmaApp.java
@@ -1,19 +1,64 @@
 package com.example.ishita.assigntasks;
 
+import android.app.Activity;
 import android.app.Application;
+import android.os.Bundle;
 
 import com.firebase.client.Firebase;
 
 /**
  * Created by ishita on 8/3/16.
  */
-public class TeamkarmaApp extends Application {
+public class TeamkarmaApp extends Application implements Application.ActivityLifecycleCallbacks {
+
+    private static boolean isCommentsActivityVisible;
 
     @Override
     public void onCreate() {
         super.onCreate();
         Firebase.setAndroidContext(this);
+        registerActivityLifecycleCallbacks(this);
     }
 
+    public static boolean isCommentsActivityVisible() {
+        return isCommentsActivityVisible;
+    }
 
+    @Override
+    public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    public void onActivityStarted(Activity activity) {
+
+    }
+
+    @Override
+    public void onActivityResumed(Activity activity) {
+        if (activity instanceof CommentsActivity)
+            isCommentsActivityVisible = true;
+    }
+
+    @Override
+    public void onActivityPaused(Activity activity) {
+
+    }
+
+    @Override
+    public void onActivityStopped(Activity activity) {
+        if (activity instanceof CommentsActivity) {
+            isCommentsActivityVisible = false;
+        }
+    }
+
+    @Override
+    public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+
+    }
+
+    @Override
+    public void onActivityDestroyed(Activity activity) {
+
+    }
 }
