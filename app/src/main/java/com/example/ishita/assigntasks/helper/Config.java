62749e090b452557877e8293d3b80b7cd96981a5
Ishita <ishiestar@gmail.com>
2016-03-29 11:05:04 +0530

    added verify_otp url

--- app/src/main/java/com/example/ishita/assigntasks/helper/Config.java
+++ app/src/main/java/com/example/ishita/assigntasks/helper/Config.java
@@ -7,7 +7,7 @@ public class Config {
     //TODO fill all the relevant info when server work is done.
     // server URL configuration
     public static final String URL_REQUEST_SMS = "http://192.168.42.110:5000/chatauthtelno";
-    public static final String URL_VERIFY_OTP = "put verify otp url here";
+    public static final String URL_VERIFY_OTP = "http://192.168.42.110:5000/chatauthtelno";
 
     // SMS provider identification
     // It should match with your SMS gateway origin
