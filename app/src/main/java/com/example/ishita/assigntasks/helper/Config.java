678d423c581c74b74f8d6b42208c9355084b72d2
Ishita <ishiestar@gmail.com>
2016-03-30 12:29:28 +0530

    finalized config file

--- app/src/main/java/com/example/ishita/assigntasks/helper/Config.java
+++ app/src/main/java/com/example/ishita/assigntasks/helper/Config.java
@@ -4,16 +4,18 @@ package com.example.ishita.assigntasks.helper;
  * This class contains very important app configuration information.
  */
 public class Config {
-    //TODO fill all the relevant info when server work is done.
     // server URL configuration
-    public static final String URL_REQUEST_SMS = "http://192.168.42.110:5000/chatauthtelno";
-    public static final String URL_VERIFY_OTP = "http://192.168.42.110:5000/chatauthtelno";
+    // change VERIFY_OTP url in case you decide to use two separate urls
+    public static final String URL_REQUEST_SMS = "http://192.168.42.110:5000/chatauth";
+    public static final String URL_VERIFY_OTP = "http://192.168.42.110:5000/chatauth";
+
+    //Firebase references
+    public static final String ROOT_REF = "https://teamkarma.firebaseio.com/";
+    public static final String LOGIN_REF = "https://teamkarma.firebaseio.com/login";
 
     // SMS provider identification
-    // It should match with your SMS gateway origin
-    // You can use  MSGIND, TESTER and ALERTS as sender ID
-    // If you want custom sender Id, approve MSG91 to get one
-    public static final String SMS_ORIGIN = "put sender ID from server here";
+    // It should match with the ID in your SMS gateway origin
+    public static final String SMS_ORIGIN = "+919895008997";
 
     // special character to prefix the otp. Make sure this character appears only once in the sms
     public static final String OTP_DELIMITER = ":";
